e7e20876a597b2efb6acd45e42227f519773bb8b
Ayslan Francisco Monteiro Neto <ayslanfrancisco999@gmail.com>
2025-03-31 10:50:11 -0300

    style: Refazendo pagina de index

--- app/__init__.py
+++ app/__init__.py
@@ -35,7 +35,7 @@ def load_user(user_id):
 
 @app.route('/')
 def index():
-    return render_template("dashboard.html")
+    return render_template("index.html")
 
 
 @app.route('/gerenciar_perfil')
